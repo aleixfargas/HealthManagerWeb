e3c6aae9882704abf3eca7fecb3f7c9f3f4f3924
Aleix <afargas92@gmail.com>
2016-10-23 23:43:27 +0200

    Patients form, addNew patient functionality in a modal

--- src/AppBundle/Controller/PatientsController.php
+++ src/AppBundle/Controller/PatientsController.php
@@ -6,11 +6,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Form\PatientsType;
+use AppBundle\Entity\Patients;
+
 class PatientsController extends Controller
 {
     private $section_name = 'base.global_section_patients';
 
-    private function getTranslatedSectionName(){
+    private function getTranslatedSectionName()
+    {
         return $this->get('translator')->trans($this->section_name, array(), 'base');
     }
     
@@ -19,8 +23,15 @@ class PatientsController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $patients = new Patients();
+        $form = $this->createForm(PatientsType::class, $patients, array(
+            'action' => $this->generateUrl('patients_save'),
+            'method' => 'GET',
+        ));
+        
         return $this->render(
             'patients/patients.html.twig', array(
+                'form' => $form->createView(),
                 'is_section' =>true,
                 'sections' => [
                     ['url'=>'#', 'name'=>$this->getTranslatedSectionName()]
@@ -29,13 +40,21 @@ class PatientsController extends Controller
         );
     }
     
+    
+    /**
+     * @Route("/patients/save", name="patients_save")
+     */
+    public function savePatient(Request $request){
+        return $this->redirect($this->generateUrl('patients'));
+    }
+
     /**
      * @Route("/patients/{patient}", name="patients_show")
      */
     public function showPatientAction($patient)
     {
         return $this->render(
-            'patients/patients_show.html.twig', array(
+            'patients/show_patients.html.twig', array(
                 'patient_id'=>$patient,
                 'is_section' =>true,
                 'sections' => [
