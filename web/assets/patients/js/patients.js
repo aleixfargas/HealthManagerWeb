1043d1bf9cb54807cb12e1964c12f7bd5e38c05b
Aleix <afargas92@gmail.com>
2016-10-26 00:48:56 +0200

    checkbox table patients and delete functionality init

--- web/assets/patients/js/patients.js
+++ web/assets/patients/js/patients.js
@@ -1,6 +1,32 @@
+var patients_selected = [];
+
 $(document).ready(function () {
-    $('.patient').click(function(){
-        var url = $(this).attr('url');
+    $('.patient > :not(.bs-checkbox)').click(function(){
+        var url = $(this).parent('tr').attr('url');
         window.location.href = url;
     })
+    
+    $('#delete_patients').click(function(){
+        show_patients_checkbox();
+        remove_click_listener('.patient > :not(.bs-checkbox)');
+        add_checkbox_listener();
+    });
 });
+
+function show_patients_checkbox(){
+    $('.check_patient').removeClass('hidden');
+}
+
+function remove_click_listener(object){
+    $(object).unbind('click');
+}
+
+function add_checkbox_listener(){
+    $('.patient').click(function(){
+        var patient_id = $(this).attr('data-index');
+        
+        $(this).toggleClass('selected');
+        $(this).children('.bs-checkbox > :checkbox').checked = true;
+        alert(patient_id);
+    })
+}
