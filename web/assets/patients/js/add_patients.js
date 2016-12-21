b607d9a7702615f00609a26d673e00d7d09a0ab3
Aleix <afargas92@gmail.com>
2016-12-21 23:42:19 +0100

    added img and js

---
+++ web/assets/patients/js/add_patients.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#form_new_patient").submit(function(e){
+        e.preventDefault();
+        var formSerialize = $(this).serialize();
+        $.post($(this).attr('url'), formSerialize, function(response){
+//            console.log(response.action);
+            if(response.status == 'success'){
+                window.location.href = response.action;
+            } else {
+                swal(
+                    'Error!',
+                    response.action,
+                    'error'
+                );
+            }
+        },'JSON');
+    });
+});
