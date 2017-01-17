0f81820b007138af0553a0617be5d06ca2a33d5f
Aleix Fargas <aleix@dc-image.com>
2017-01-17 17:59:54 +0100

    New feature, scroll-to-top function in all base-menu

--- web/assets/base/js/base.js
+++ web/assets/base/js/base.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {
+    $(document).on( 'scroll', function(){
+    	if ($(window).scrollTop() > 100) {
+            add_scroll_to_top();
+        } else {
+            remove_scroll_to_top();
+        }
+    });
+});
+
 function array_pop(array, removeItem){ 
     array = jQuery.grep(array, function(new_array) {
         return new_array != removeItem;
@@ -12,4 +22,15 @@ function rm_click_listener(object){
 
 function get_locale(){
     return $("#base_locale").val();
+}
+
+function add_scroll_to_top(){
+    $('.scroll-top-wrapper').addClass('show');
+    $('.scroll-top-wrapper').click(function(){
+        window.scroll({ top: 0, left: 0, behavior: 'smooth' });    
+    });
+}
+
+function remove_scroll_to_top(){
+    $('.scroll-top-wrapper').removeClass('show');
 }
