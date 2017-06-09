61e0636206cd37144a0da2c9493e5b6bfe27288e
Aleix <afargas92@gmail.com>
2017-06-09 22:44:24 +0200

    setting homepage visits-list

--- src/AppBundle/Controller/VisitsController.php
+++ src/AppBundle/Controller/VisitsController.php
@@ -38,7 +38,7 @@ class VisitsController extends Controller{
     }
     
     /**
-     * @Route("/{day}", name="homepage")
+     * @Route("/visits/list/{day}", name="visits-list")
     */
     public function listVisitsAction($day = false){
         if($day === false) $day = date("Y-m-d");
