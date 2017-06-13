34f776d31055f2179db94e76f3ba257a400e0ab2
Aleix <afargas92@gmail.com>
2017-06-13 14:14:43 +0200

    moment forcing date formats

--- web/assets/visits/js/visits.js
+++ web/assets/visits/js/visits.js
@@ -139,7 +139,7 @@ function add_show_visit_listener(){
         var visitHour = button.data('hour') + ':00';
         
         alert(visitDay);
-        var visit_dateTime = moment(visitDay, "YYYY-MM-DD");
+        var visit_dateTime = moment(visitDay, ["YYYY-MM-DD"]);
         console.log(visit_dateTime);
         visit_dateTime.hour(visitHour);
         var visit_dateTimeString = visit_dateTime.format('YYYY-MM-DD HH:mm:ss');
