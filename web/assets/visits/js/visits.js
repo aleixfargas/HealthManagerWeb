1a2f6518809ddbc9b11ebc76d210212b4deae118
Aleix <afargas92@gmail.com>
2017-06-16 22:55:12 +0200

    clear white spaces from pone

--- web/assets/visits/js/visits.js
+++ web/assets/visits/js/visits.js
@@ -222,22 +222,19 @@ function add_show_visit_listener(){
             if(arrayData.length > 3 && arrayData[3].name == 'phone'){
                 arrayData[3].value.replace(/\s/g,'');
             }
-            if(arrayData.length > 3 && arrayData[3].name == 'phone' && arrayData[3].value.length > 11){
-                swal('Error', Translator.trans('incorrect_phone_length'), 'error');                            
-            } else {
-                $.post($(this).attr('url'), formSerialize, function(response){
-                    if(response.status == 'success'){
+            
+            $.post($(this).attr('url'), formSerialize, function(response){
+                if(response.status == 'success'){
 //                    window.location.href = response.action_listVisits;
-                        go_to_date(visitDay);
-                        modal.modal('hide')
-                    } else {
-                        swal('Error', response.action, 'error');
-                    }
-                },'JSON').fail(function() {
-                    swal('Error', 'OUPS!, Something went incredibly wrong adding the new visit...', 'error');
-                    console.log('OUPS!, Something went incredibly wrong adding the new visit...');
-                });
-            }
+                    go_to_date(visitDay);
+                    modal.modal('hide')
+                } else {
+                    swal('Error', response.action, 'error');
+                }
+            },'JSON').fail(function() {
+                swal('Error', 'OUPS!, Something went incredibly wrong adding the new visit...', 'error');
+                console.log('OUPS!, Something went incredibly wrong adding the new visit...');
+            });
 
             e.preventDefault();
         });
